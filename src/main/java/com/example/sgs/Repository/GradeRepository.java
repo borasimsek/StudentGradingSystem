c21a664c809e43ecaebdb15956df89ffdc10174d
Murat <yigit.mert@ozu.edu.tr>
2024-12-21 04:24:58 +0300

    Berk's GradeRepository committed again.

---
+++ src/main/java/com/example/sgs/Repository/GradeRepository.java
@@ -0,0 +1,11 @@
+package com.example.sgs.Repository;
+
+import java.util.List;
+
+public interface GradeRepository {
+
+    void saveGrade(String studentID, String courseID, double grade);
+    double findGrade(String studentID, String courseID);
+    // New method to fetch all grades for a specific student
+    List<Double> findGradesByStudentId(String studentID);
+}
